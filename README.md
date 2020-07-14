5f4501ccd71a3d050ebf598d50934a69fa3ec668
Naeem ul Hassan <naeemhassan09@gmail.com>
2020-07-14 15:04:51 +0500

    first commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Resume-APP 
+[![Build Status](https://travis-ci.org/naeemhassan09/naeemulhassan.svg?branch=master)](https://travis-ci.org/naeemhassan09/naeemulhassan)
+
+
+- To run this project follow these steps , 
+  - clone the repo using `git clone`
+  - navigate to folder `Resume-app`
+  - run `npm install` to instal node modules
+  - run `npm start` to start the service on `localhost:3000`
+    
+##### deployed app : http://www.naeemulhassan.com/
---
+++ src/components/timeline.jsx
@@ -0,0 +1,65 @@
+import React, { Component } from 'react'
+
+export default class Timeline extends Component {
+  render() {
+    return (
+      <div>
+        <section className="colorlib-experience" data-section="timeline">
+          <div className="colorlib-narrow-content">
+            <div className="row">
+              <div className="col-md-6 col-md-offset-3 col-md-pull-3 animate-box" data-animate-effect="fadeInLeft">
+                <span className="heading-meta">highlights</span>
+                <h2 className="colorlib-heading animate-box">Timeline</h2>
+              </div>
+            </div>
+            <div className="row">
+              <div className="col-md-12">
+                <div className="timeline-centered">
+                  <article className="timeline-entry animate-box" data-animate-effect="fadeInLeft">
+                    <div className="timeline-entry-inner">
+                      <div className="timeline-icon color-3">
+                        <i className="icon-pen2" />
+                      </div>
+                      <div className="timeline-label">
+                        <h2> MBA (Supply Chain management) at Barhria University <span>2014-2016</span></h2>
+                        <p>I have completed Master in Business studies from Bahria University in 2019. I have taken supply chain management as my major, the objective is to help me through an industrial job. </p>
+                      </div>
+                    </div>
+                  </article>
+                  <article className="timeline-entry animate-box" data-animate-effect="fadeInTop">
+                    <div className="timeline-entry-inner">
+                      <div className="timeline-icon color-4">
+                        <i className="icon-pen2" />
+                      </div>
+                      <div className="timeline-label">
+                        <h2>Under-graduation at NUCES-FAST-Lahore<span>2009-2013</span></h2>
+                        <p>I have completed my under-graduation studies with major in Electrical Engineering(Computer & Embedded). I have taken the various computer and embedded courses during the study. I have done my FYP based on controlled system</p>
+                      </div>
+                    </div>
+                  </article>
+                  <article className="timeline-entry animate-box" data-animate-effect="fadeInLeft">
+                    <div className="timeline-entry-inner">
+                      <div className="timeline-icon color-5">
+                        <i className="icon-pen2" />
+                      </div>
+                      <div className="timeline-label">
+                        <h2>Primary and Higher Education <span>2007-2009</span></h2>
+                        <p>I have completed my higher secondary education with major subjects as Physics,Chemistry & Maths with 76% merit. During my time at college, I have developed interest in solving complex problems of the fundamental physics which helped me to improve my understanding of any problem and also my mathematical skills to actually solve the problems.</p> 
+                      </div>
+                    </div>
+                  </article>
+                  <article className="timeline-entry begin animate-box" data-animate-effect="fadeInBottom">
+                    <div className="timeline-entry-inner">
+                      <div className="timeline-icon color-none">
+                      </div>
+                    </div>
+                  </article>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+      </div>
+    )
+  }
+}
